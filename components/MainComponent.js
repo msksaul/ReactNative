9806c3f39c16b6a0082df9223e6ae8d58419d81c
msksaul <64514098+msksaul@users.noreply.github.com>
2020-07-30 23:25:29 -0600

    React Native Navigation Part 2

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
+import Home from './HomeComponent';
 import Menu from './MenuComponent';
 import DishDetail from './DishDetailComponent';
 import { createStackNavigator } from '@react-navigation/stack';
-import { NavigationContainer } from '@react-navigation/native'
+import { NavigationContainer, DrawerActions } from '@react-navigation/native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 
 const MenuNavigator = createStackNavigator();
 
@@ -32,11 +34,44 @@ function MenuNavigatorScreen() {
   );
 }
 
+const HomeNavigator = createStackNavigator();
+
+function HomeNavigatorScreen() {
+  return(
+      <HomeNavigator.Navigator
+          screenOptions={{
+              headerStyle: {
+                  backgroundColor: "#512DA8"
+              },
+              headerTintColor: "#fff",
+              headerTitleStyle: {
+                  color: "#fff"            
+              }
+          }}>
+          <HomeNavigator.Screen
+              name="Home"
+              component={Home}
+          />            
+      </HomeNavigator.Navigator>
+  );
+}
+
+const MainNavigator = createDrawerNavigator();
+
+function MainNavigatorScreen() {
+  return(
+    <MainNavigator.Navigator drawerStyle={{ backgroundColor: '#D1C4E9' }}>
+      <MainNavigator.Screen name='Home' component={HomeNavigatorScreen}/>
+      <MainNavigator.Screen name='Menu' component={MenuNavigatorScreen}/>
+    </MainNavigator.Navigator>
+  )
+}
+
 class Main extends Component {
   render() {
     return(
       <NavigationContainer>
-          <MenuNavigatorScreen/>           
+          <MainNavigatorScreen/>           
       </NavigationContainer>
     )
   }
