32b9474bf8bbb479850ae39459c88b02b0012440
msksaul <64514098+msksaul@users.noreply.github.com>
2020-08-11 17:42:41 -0600

    Gestures Part 2 (Not worked due to outdated gestures content)

--- components/DishDetailComponent.js
+++ components/DishDetailComponent.js
@@ -23,6 +23,8 @@ function RenderDish(props) {
 
   const dish = props.dish;
 
+  const handleViewRef = ref => this.view = ref
+
   const recognizeDrag = ({moveX, moveY, dx, dy}) => {
     if (dx < -200)
       return true
@@ -33,6 +35,10 @@ function RenderDish(props) {
   const panResponder = PanResponder.create({
     onStartShouldSetPanResponder: (e, gestureState) => {
       return true
+    },
+    onPanResponderGrant: () => {
+      this.view.rubberBand(1000)
+        .then(endState => console.log(endState.finished ? 'finished' : 'cancelled'))
     },
     onPanResponderEnd: (e, gestureState) => {
       if (recognizeDrag(gestureState))
@@ -59,6 +65,7 @@ function RenderDish(props) {
   if (dish != null) {
     return(
       <Animatable.View animation='fadeInDown' duration={2000} delay={1000}
+        ref={handleViewRef}
         {...panResponder.panHandlers}>
         <Card
           featuredTitle={dish.name}
