374ff1bf058d8aaafbb36aab52fa2d2438c7de33
msksaul <64514098+msksaul@users.noreply.github.com>
2020-08-13 18:31:54 -0600

    Image Manipulator

--- components/LoginComponent.js
+++ components/LoginComponent.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
-import { View, StyleSheet, Text, ScrollView, Image } from 'react-native';
+import { View, StyleSheet, ScrollView, Image } from 'react-native';
 import { Icon, Input, CheckBox, Button } from 'react-native-elements';
 import * as SecureStore from 'expo-secure-store';
 import * as Permissions from 'expo-permissions';
-import * as ImagePicker from 'expo-image-picker'
+import * as ImagePicker from 'expo-image-picker';
+import * as ImageManipulator from 'expo-image-manipulator';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { baseUrl } from '../shared/baseUrl';
 
@@ -108,9 +109,20 @@ class RegisterTab extends Component {
       })
 
       if (!capturedImage.cancelled) {
-        this.setState({imageUrl: capturedImage.uri})
+        this.processImage(capturedImage.uri)
       }
     }
+  }
+
+  processImage = async (imageUri) => {
+    let processedImage = await ImageManipulator.manipulateAsync(
+      imageUri,
+      [
+        {resize: {width: 400}}
+      ],
+      {format: 'png'}
+    )
+    this.setState({imageUrl: processedImage.uri})
   }
 
   handleRegister() {
